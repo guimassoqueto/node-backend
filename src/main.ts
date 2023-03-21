209383d8d74db1dac5cd0d7f265146ebff90538a
gmassoqueto <gmassoqueto@stone.com.br>
2023-03-21 16:59:22 -0300

    body-parser adicionado

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { APP_PORT } from './settings';
 
 const app = express();
 
+// body-parser
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.listen(APP_PORT, () => {
   console.log(`Example app listening on port ${APP_PORT}`);
