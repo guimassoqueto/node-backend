58d07fa3375f6dc5c57cf14b277b77f40692b6cd
gmassoqueto <gmassoqueto@stone.com.br>
2023-03-22 09:37:39 -0300

    update setCors function

--- src/middlewares/setCors.middleware.ts
+++ src/middlewares/setCors.middleware.ts
@@ -5,7 +5,7 @@ import { Request, Response, NextFunction } from 'express';
  */
 export default function setCors(req: Request, res: Response, next: NextFunction) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-COntrol-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-COntrol-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 }
