eaa095ea536c165c132cff218a5af0584021f13e
gmassoqueto <gmassoqueto@stone.com.br>
2023-03-25 10:10:04 -0300

    upload updated

--- src/controllers/uploads.controller.ts
+++ src/controllers/uploads.controller.ts
@@ -2,20 +2,21 @@ import { NextFunction, Request, Response } from "express";
 import CustomError from "../errors/CustomError.error";
 import { Status } from "../enums/statusCodes.enum";
 import { S3Service } from "../cloud/s3Service";
+import { ErrorMessage } from "../enums/errorMessages.enum";
 
 
 export async function postUpload(req: Request, res: Response, next: NextFunction) {
   try {
     const { file } = req;
-    if (!file) return res.status(Status.BadRequest).json({status: "invalid file format"});
+    if (!file) return next(new CustomError(Status.BadRequest, ErrorMessage.InvalidFileFormat));;
     
     const s3 = new S3Service(file);
     await s3.upload();
 
-    return res.status(Status.OK).json({status: "file uploaded", url: s3.S3FileLocation});
+    return res.status(Status.OK).json({fileLocationURL: s3.S3FileLocation});
     
   } catch (error) {
     console.error(error);
-    next(new CustomError(Status.ServiceUnavailable, "Um erro ocorreu, tente mais tarde"))
+    next(new CustomError(Status.ServiceUnavailable, ErrorMessage.Generic))
   }
 }
