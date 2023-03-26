b5a63753d60f36a7eb17335e6c70522b6d10a94c
gmassoqueto <gmassoqueto@stone.com.br>
2023-03-26 09:41:03 -0300

    users routes updated now passing new middlewares

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getUser, getAllUsers, postUser, updateUser, deleteUser } from "../controllers/users.controller";
 import validateRequestBody from "../middlewares/validateRequestBody.middleware";
+import checkRequestBodyKeys from "../middlewares/checkRequestBodyKeys.middleware";
 
 export const usersRoute = Router();
 
 usersRoute.get("/", getAllUsers);
-usersRoute.post("/", validateRequestBody, postUser);
+usersRoute.post("/", checkRequestBodyKeys(["name", "email", "password"]), validateRequestBody, postUser);
 usersRoute.get('/:id', getUser);
-usersRoute.put('/:id', validateRequestBody, updateUser);
+usersRoute.put('/:id', checkRequestBodyKeys(["name", "email", "password", "new_password"]), validateRequestBody, updateUser);
 usersRoute.delete('/:id', deleteUser)
