2bb27dcf408fc2702b6d45761d4591dd59c1a8a8
gmassoqueto <gmassoqueto@stone.com.br>
2023-03-27 11:37:12 -0300

    add auth to user routes

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { getUser, getAllUsers, postUser, updateUser, deleteUser } from "../controllers/users.controller";
 import validateRequestBody from "../middlewares/validateRequestBody.middleware";
 import checkRequestBodyKeys from "../middlewares/checkRequestBodyKeys.middleware";
+import isAuth from "../middlewares/isAuth.middleware";
 
 export const usersRoute = Router();
 
-usersRoute.get("/", getAllUsers);
+usersRoute.get("/", isAuth, getAllUsers);
 usersRoute.post("/", checkRequestBodyKeys(["name", "email", "password"]), validateRequestBody, postUser);
-usersRoute.get('/:id', getUser);
 usersRoute.put('/:id', checkRequestBodyKeys(["name", "email", "password"]), validateRequestBody, updateUser);
+usersRoute.get('/:id', getUser);
 usersRoute.delete('/:id', deleteUser)
