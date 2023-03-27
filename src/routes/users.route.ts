885842644f5042a6866ba6e147d6b0c0736ca44e
gmassoqueto <gmassoqueto@stone.com.br>
2023-03-27 12:01:38 -0300

    update middleware name in route

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { getUser, getAllUsers, postUser, updateUser, deleteUser } from "../controllers/users.controller";
 import validateRequestBody from "../middlewares/validateRequestBody.middleware";
-import checkRequestBodyKeys from "../middlewares/checkRequestBodyKeys.middleware";
 import isAuth from "../middlewares/isAuth.middleware";
+import requiredReqBodyKeys from "../middlewares/requiredReqBodyKeys.middleware";
 
 export const usersRoute = Router();
 
 usersRoute.get("/", isAuth, getAllUsers);
-usersRoute.post("/", checkRequestBodyKeys(["name", "email", "password"]), validateRequestBody, postUser);
-usersRoute.put('/:id', checkRequestBodyKeys(["name", "email", "password"]), validateRequestBody, updateUser);
+usersRoute.post("/", requiredReqBodyKeys(["name", "email", "password"]), validateRequestBody, postUser);
+usersRoute.put('/:id', requiredReqBodyKeys(["name", "email", "password"]), validateRequestBody, updateUser);
 usersRoute.get('/:id', getUser);
 usersRoute.delete('/:id', deleteUser)
