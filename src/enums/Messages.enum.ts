d6ecc2e23473a949808aabe8209f062921ae9ff2
gmassoqueto <gmassoqueto@stone.com.br>
2023-03-27 11:12:16 -0300

    enum de mensagens

---
+++ src/enums/Messages.enum.ts
@@ -0,0 +1,10 @@
+export enum Message {
+  GenericError = "Something wrong happened, please try again later",
+  ResourceNotFound = "Resource not found",
+  InvalidFileFormat = "Invalid file format",
+  InvalidRequestBody = "Invalid request body",
+  EmailAlreadyInUse = "Email already in use",
+  InvalidPassword = "Password does not match",
+  AccessForbidden = "Access forbidden",
+  NotAuthenticated = "User not authenticated"
+}
--- src/enums/errorMessages.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum ErrorMessage {
-  Generic = "Something wrong happened, please try again later",
-  NotFound = "Resource not found",
-  InvalidFileFormat = "Invalid file format",
-  InvalidRequestBody = "Invalid request body",
-  EmailAlreadyInUse = "Email already in use",
-  InvalidPassword = "Password does not match"
-}
