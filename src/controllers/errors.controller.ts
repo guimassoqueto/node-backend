26a3e09b11f3678c2712d284f611c8abc5fbcabc
gmassoqueto <gmassoqueto@stone.com.br>
2023-03-28 08:28:46 -0300

    adiciona JSDoc

--- src/controllers/errors.controller.ts
+++ src/controllers/errors.controller.ts
@@ -3,6 +3,10 @@ import CustomError from "../errors/CustomError.error";
 import { Status } from "../enums/statusCodes.enum";
 import { Message } from "../enums/Messages.enum";
 
+/**
+ * 
+ * Rota de teste que retorna erro sempre que for chamada, utilizada para fins de teste
+ */
 export function getError(req: Request, res: Response, next: NextFunction) {
   try {
     const error = new CustomError(Status.ServiceUnavailable, Message.GenericError);
