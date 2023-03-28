fd02594196c9f14c4229d7195936812c3daf455c
gmassoqueto <gmassoqueto@stone.com.br>
2023-03-28 08:26:55 -0300

    adiciona jSDoc na função

--- src/middlewares/errorManager.middleware.ts
+++ src/middlewares/errorManager.middleware.ts
@@ -1,5 +1,13 @@
-import { Errback, Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 
+/**
+ * Middleware final que trata os erros na aplicação
+ * @param error erro ocorrido durante determinada etapa do processo, geralmente em controllers
+ * @param req a requisição do cliente
+ * @param res a resposta ao cliente
+ * @param next next
+ * @returns status + mensagen de retorno ao cliente
+ */
 export default function errorManager(error: any, req: Request, res: Response, next: NextFunction) {
   if (error) return res.status(error?.statusCode).json({
     message: error?.message,
